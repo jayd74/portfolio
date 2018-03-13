622fb1a7aa74b0fbea0c728236b65fa15379c881
Jason Du <jayd74@gmail.com>
2018-03-13 17:32:02 -0400

    added smooth scroll and restyled css

--- js/main.js
+++ js/main.js
@@ -2,6 +2,33 @@
 
 var myWork = {};
 
+myWork.smoothScroll = function () {
+    $("#toProjects").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#projects").offset().top
+        }, 1000);
+    });
+
+    $("#toAbout").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#about").offset().top
+        }, 1000);
+    });
+
+    $("#toContact").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#contact").offset().top
+        }, 1000);
+    });
+
+    $("#toTop").click(function () {
+        $("html, body").animate({
+            scrollTop: $("#top").offset().top
+        }, 1000);
+    });
+};
+
 $(function () {
     AOS.init();
+    myWork.smoothScroll();
 });
