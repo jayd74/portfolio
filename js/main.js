5fe7665267cb9f09400f02e21e9606c0f5789eb5
Jason Du <jayd74@gmail.com>
2018-03-13 12:11:59 -0400

    fixed styling for responsive design

--- js/main.js
+++ js/main.js
@@ -2,21 +2,8 @@
 
 var myWork = {};
 
-// myWork.getWakaTime = (wakaTime) => {
-//     return $.ajax({
-//         type: 'GET',
-//         url: 'https://wakatime.com/share/@jayd74/71d994ec-2f76-4774-afe8-3478cb28eb0a.json',
-//         dataType: 'jsonp',
-//         success: function (response) {
-//             console.log(response.data);
-//         },
-//     });
-// }
-
-myWork.init = function () {
-    // myWork.getWakaTime();
-};
+myWork.init = function () {};
 
 $(function () {
-    myWork.init();
+    $(".fa-arrow-circle-down").scrollTop(300);
 });
--- js/script.js
+++ js/script.js
@@ -1,20 +1,11 @@
 const myWork = {};
 
-// myWork.getWakaTime = (wakaTime) => {
-//     return $.ajax({
-//         type: 'GET',
-//         url: 'https://wakatime.com/share/@jayd74/71d994ec-2f76-4774-afe8-3478cb28eb0a.json',
-//         dataType: 'jsonp',
-//         success: function (response) {
-//             console.log(response.data);
-//         },
-//     });
-// }
+
 
 myWork.init = function (){
-    // myWork.getWakaTime();
 }
 
 $(function () {
-    myWork.init();
+    $(".fa-arrow-circle-down").scrollTop(300);
+    
 })
